122995066362f3934764a94378957f3b3f435b48
321nomyzS <balinskiszymon@gmail.com>
2025-03-22 23:39:34 +0100

    Extend models with created_by

--- dashboard/models.py
+++ dashboard/models.py
@@ -19,6 +19,7 @@ class Tag(models.Model):
     name = models.CharField(max_length=50)
     category = models.ForeignKey(TagCategory, on_delete=models.CASCADE, related_name='tags')
     image = models.ImageField(upload_to=exercise_tag_directory_path, blank=True, null=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     class Meta:
         unique_together = ('name', 'category')
@@ -50,6 +51,7 @@ class Exercise(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.title
